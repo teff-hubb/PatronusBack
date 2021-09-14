347bc5b37dc748ef073899f7fc21f975aa3c82cc
teff-hubb <stepxan@yahoo.es>
2021-09-14 12:36:23 +0200

    Elimine un codigo inutil en app

--- app.js
+++ app.js
@@ -28,11 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-  secret: 'SECRET',
-  resave: true,
-  saveUninitialized: true
-}));
+
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 app.use('/api', apiRouter);
